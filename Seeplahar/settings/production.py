1ad315069b076c64f31947fc0801dd4c8054d2dc
rco3 <githubuser@rco3.com>
2024-05-28 01:22:42 -0400

    renaming things to lower case, hoping that will fix things.

---
+++ Seeplahar/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+from decouple import Csv
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
